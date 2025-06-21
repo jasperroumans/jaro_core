169611ecd9fe86bd2eaa2430342b412302b4f591
jasperroumans <jasper.roumans@gmail.com>
2025-06-21 10:26:29 +0200

    Add context switching system

---
+++ modules/highlight_module.py
@@ -0,0 +1,11 @@
+"""Highlight module voor JARO-TOOTH.
+
+Deze eenvoudige variant drukt enkel een bevestiging bij het starten
+en kan later uitgebreid worden met echte functionaliteit.
+"""
+
+
+def start() -> None:
+    """Start de highlight module."""
+    print("\U0001F3A5 Highlight module gestart")
+
